b9ec9932aabc6b3d90dae89761d3d535fe4067b7
Ayub Shrestha <ayubsht1@gmail.com>
2025-05-05 21:23:25 +0545

    start up

---
+++ components/Nav.jsx
@@ -0,0 +1,34 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+    {
+        name: "home",
+        path: "/",
+    },
+    {
+        name: "services",
+        path: "/services",
+    },
+    {
+        name: "resume",
+        path: "/resume",
+    },
+    {
+        name: "work",
+        path: "/work",
+    },
+    {
+        name: "contact",
+        path: "/contact",
+    }
+]
+
+const Nav = () => {
+  return (
+    <nav>{links.map((link,index)=>{})}</nav>
+  )
+}
+
+export default Nav
