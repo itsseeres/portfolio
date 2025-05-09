edd2ae3471cf8ed7ec88e1e27428bda35b483261
Ayub Shrestha <ayubsht1@gmail.com>
2025-05-10 00:34:15 +0545

    resume finish

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -171,9 +171,45 @@ const Resume = () => {
                 </ScrollArea>
               </div>
             </TabsContent>
-            <TabsContent value="skills" className="w-full"> 
-              skills</TabsContent>
-            <TabsContent value="about" className="w-full"> about</TabsContent>
+            <TabsContent value="skills" className="w-full h-full"> 
+            <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{skills.description}</p>
+                  <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                    {skills.skillList.map((skill,index)=>{
+                      return (<li key={index} >
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                              <div className="text-6xl group-hover:text-cyan-400 transition-all duration-300">{skill.icon}</div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p className="capitalize">{skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>);
+                    })}
+                  </ul>
+                </div>
+            </div>
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
+                  <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-7 max-w-[620px] mx-auto xl:mx-0">
+                    {about.info.map((item,index)=>{
+                      return (<li key={index} className="flex items-center justify-center xl:justify-start gap-4">
+                          <span className="text-cyan-400">{item.fieldName}</span>
+                          <span className="text-xl">{item.fieldValue}</span>
+
+                      </li>);
+                    })}
+                  </ul>
+              </div>
+            </TabsContent>
           </div>
         </Tabs>
       </div>
