bf37ab178b22b4801f3ac2ad207b4fd3e8c0c4b0
Ayub Shrestha <ayubsht1@gmail.com>
2025-05-10 23:47:30 +0545

    remaining from slider buttons

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -29,7 +29,7 @@ const Work = () => {
   setProject(projects[currentIndex])
   }
   return (
-    <motion.div initial={{opacity:0}} animate={{opacity:1}} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+    <motion.div initial={{opacity:0}} animate={{opacity:1, transition:{delay:2.4, duration:0.4, ease:"easeIn"}}} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
     <div className="container mx-auto">
       <div className="flex flex-col xl:flex-row xl:gap-[30px]">
         <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
@@ -81,12 +81,15 @@ const Work = () => {
             return(
               <SwiperSlide key={index} className="w-full">
                 <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                <div></div>
+
+                <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                 <div className="relative w-full h-full"><Image src={project.image} fill className="object-cover" alt=""/></div>
                 </div>
               </SwiperSlide>
             )
-          })}</Swiper>
+          })}
+          
+          </Swiper>
         </div>
       </div>
     </div>
