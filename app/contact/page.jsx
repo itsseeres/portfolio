a7f3c5c6e93e1aa9c3448a3ccef290b4be331864
Ayub Shrestha <ayubsht1@gmail.com>
2025-05-17 00:01:52 +0545

    final

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -43,9 +43,9 @@ const Contact = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* Contact Form */}
-          <div className="xl:h-[54%] order-2 xl:order-none flex-1">
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-primary rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-cyan-400">Let's work together</h3>
               <p className="text-white/60">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis nesciunt quasi
                 doloribus dolorem mollitia.
@@ -70,21 +70,31 @@ const Contact = () => {
                 </SelectContent>
               </Select>
               <Textarea placeholder="Write your message..." className="h-[200px]" />
-              <Button type="submit">Send Message</Button>
+              <Button size="md"  className="max-w-40" type="submit">Send Message</Button>
             </form>
           </div>
 
           {/* Contact Info */}
-          <div className="flex-1 flex flex-col gap-6 items-start justify-center xl:items-end order-1 xl:order-none mb-8 xl:mb-0 text-white">
-            {info.map((item, index) => (
-              <div key={index} className="flex items-start gap-4">
-                <div className="text-accent text-xl mt-1">{item.icon}</div>
-                <div>
-                  <h4 className="font-semibold">{item.title}</h4>
-                  <p className="text-sm text-white/60">{item.description}</p>
+          <div className="flex-1 flex items-center justify-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+
+            {info.map((item, index) => {
+              return(
+
+                <li key={index} className="flex items-center gap-7">
+                <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-cyan-400 rounded-md flex items-center justify-center">
+                  <div className="text-[28px]">
+                  {item.icon}
+                  </div>
+                  </div>
+                <div className="flex-1">
+                  <p className="text-white/60">{item.title}</p>
+                  <h3 className="text-xl">{item.description}</h3>
                 </div>
-              </div>
-            ))}
+              </li>
+              )
+})}
+            </ul>
           </div>
         </div>
       </div>
